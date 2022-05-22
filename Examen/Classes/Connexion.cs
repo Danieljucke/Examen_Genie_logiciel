c19f72763381530bd483ada3b0a7d4893b135574
Joy Daniel <88509670+Danieljucke@users.noreply.github.com>
2022-05-22 13:48:04 +0100

    Add files via upload

--- Examen/Classes/Connexion.cs
+++ Examen/Classes/Connexion.cs
@@ -2,27 +2,51 @@
 using System.Collections.Generic;
 using System.Text;
 using System.Data.SqlClient;
+using System.Windows.Forms;
 namespace Examen.Classes
 {
     class Connexion
     {
+        protected string req;
+        Toolkit u = new Toolkit();
         public string connexion (string utilisateur)
         {
             string recup="";
-            string req = "select mot_de_passe from table connexion where utilisateur='" + utilisateur + "'";
-            Toolkit u = new Toolkit();
+            req = "select mot_de_passe from table connexion where utilisateur='" + utilisateur + "'";
             u.connexionBaseDD();
             u.commandeBDD(req);
-            SqlDataReader retourner = u._cmd.ExecuteReader(); 
-            while (retourner.Read())
+            try
             {
-                recup = retourner.GetString(0);
+                SqlDataReader retourner = u._cmd.ExecuteReader();
+                while (retourner.Read())
+                {
+                    recup = retourner.GetString(0);
+                }
             }
+            catch(Exception ex)
+            {
+                MessageBox.Show(""+ex);
+            }
+           
             return recup; 
         }
-        public void inscription()
+        public void inscription(string username, string motDePasse, string confirmMotDePasse)
         {
-            
+            req= "insert into _ values ('" + username + "','" + motDePasse + "')";
+            u.connexionBaseDD();
+            u.commandeBDD(req);
+            try
+            {
+                int chekRespondBDD = u._cmd.ExecuteNonQuery();
+                if (chekRespondBDD > 1)
+                    MessageBox.Show("Inscription Reussi");
+                else
+                    MessageBox.Show("Il existe déjà un utilsateur à ce nom ");
+            }
+            catch(Exception ex )
+            {
+                MessageBox.Show("" + ex);
+            }
         }
     }
 }

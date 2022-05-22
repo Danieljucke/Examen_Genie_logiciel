8c21dd4e54ddfa017cbc741a6ee3ca5e8bfcb048
Joy Daniel <88509670+Danieljucke@users.noreply.github.com>
2022-05-22 13:46:24 +0100

    Add files via upload

---
+++ Examen/Examen/Classes/Connexion.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Data.SqlClient;
+using System.Windows.Forms;
+namespace Examen.Classes
+{
+    class Connexion
+    {
+        protected string req;
+        Toolkit u = new Toolkit();
+        public string connexion (string utilisateur)
+        {
+            string recup="";
+            req = "select mot_de_passe from table connexion where utilisateur='" + utilisateur + "'";
+            u.connexionBaseDD();
+            u.commandeBDD(req);
+            try
+            {
+                SqlDataReader retourner = u._cmd.ExecuteReader();
+                while (retourner.Read())
+                {
+                    recup = retourner.GetString(0);
+                }
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show(""+ex);
+            }
+           
+            return recup; 
+        }
+        public void inscription(string username, string motDePasse, string confirmMotDePasse)
+        {
+            req= "insert into _ values ('" + username + "','" + motDePasse + "')";
+            u.connexionBaseDD();
+            u.commandeBDD(req);
+            try
+            {
+                int chekRespondBDD = u._cmd.ExecuteNonQuery();
+                if (chekRespondBDD > 1)
+                    MessageBox.Show("Inscription Reussi");
+                else
+                    MessageBox.Show("Il existe déjà un utilsateur à ce nom ");
+            }
+            catch(Exception ex )
+            {
+                MessageBox.Show("" + ex);
+            }
+        }
+    }
+}

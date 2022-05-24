6ae7a35f4e23fe7c0d6bbf71f19587961bcaa11a
Joy Daniel <88509670+Danieljucke@users.noreply.github.com>
2022-05-24 22:54:19 +0100

    Add files via upload

--- Examen/examen genie logiciel.sql
+++ Examen/examen genie logiciel.sql
@@ -28,20 +28,17 @@ CREATE TABLE Salle(
 	id_salle     INT  NOT NULL ,
 	Nom_Salle    VARCHAR (20) NOT NULL ,
 	Type_Salle   VARCHAR (20) NOT NULL ,
-	Capacite     INT  NOT NULL ,
-	id_cours     INT  NOT NULL  ,
-	CONSTRAINT Salle_PK PRIMARY KEY (id_salle) ,
-	CONSTRAINT Salle_AK UNIQUE (id_cours)
+	Capacite     INT  NOT NULL  ,
+	CONSTRAINT Salle_PK PRIMARY KEY (id_salle)
 );
 
 
 /*------------------------------------------------------------
--- Table: Option
+-- Table: Options
 ------------------------------------------------------------*/
-CREATE TABLE Option(
-	id_option    INT  NOT NULL ,
+CREATE TABLE Options(
 	Nom_option   VARCHAR (30) NOT NULL  ,
-	CONSTRAINT Option_PK PRIMARY KEY (id_option)
+	CONSTRAINT Options_PK PRIMARY KEY (Nom_option)
 );
 
 
@@ -49,9 +46,8 @@ CREATE TABLE Option(
 -- Table: parcours
 ------------------------------------------------------------*/
 CREATE TABLE parcours(
-	id_parcours    INT  NOT NULL ,
 	nom_parcours   VARCHAR (10) NOT NULL  ,
-	CONSTRAINT parcours_PK PRIMARY KEY (id_parcours)
+	CONSTRAINT parcours_PK PRIMARY KEY (nom_parcours)
 );
 
 
@@ -70,12 +66,12 @@ CREATE TABLE etudiant(
 	Email               VARCHAR (50) NOT NULL ,
 	Frais               INT  NOT NULL ,
 	AnneeObtentionBac   VARCHAR (15) NOT NULL ,
-	id_option           INT  NOT NULL ,
-	id_parcours         INT  NOT NULL  ,
+	Nom_option          VARCHAR (30) NOT NULL ,
+	nom_parcours        VARCHAR (10) NOT NULL  ,
 	CONSTRAINT etudiant_PK PRIMARY KEY (CNE)
 
-	,CONSTRAINT etudiant_Option_FK FOREIGN KEY (id_option) REFERENCES Option(id_option)
-	,CONSTRAINT etudiant_parcours0_FK FOREIGN KEY (id_parcours) REFERENCES parcours(id_parcours)
+	,CONSTRAINT etudiant_Options_FK FOREIGN KEY (Nom_option) REFERENCES Options(Nom_option)
+	,CONSTRAINT etudiant_parcours0_FK FOREIGN KEY (nom_parcours) REFERENCES parcours(nom_parcours)
 );
 
 
@@ -83,10 +79,9 @@ CREATE TABLE etudiant(
 -- Table: Classe
 ------------------------------------------------------------*/
 CREATE TABLE Classe(
-	id_classe       INT  NOT NULL ,
 	nom_classe      VARCHAR (20) NOT NULL ,
 	niveau_classe   VARCHAR (20) NOT NULL  ,
-	CONSTRAINT Classe_PK PRIMARY KEY (id_classe)
+	CONSTRAINT Classe_PK PRIMARY KEY (nom_classe)
 );
 
 
@@ -94,14 +89,13 @@ CREATE TABLE Classe(
 -- Table: Module
 ------------------------------------------------------------*/
 CREATE TABLE Module(
-	id_module          INT  NOT NULL ,
 	Nom_Module         VARCHAR (30) NOT NULL ,
 	Moyenne_generale   INT  NOT NULL ,
 	charge_horaire     INT  NOT NULL ,
-	id_classe          INT  NOT NULL  ,
-	CONSTRAINT Module_PK PRIMARY KEY (id_module)
+	nom_classe         VARCHAR (20) NOT NULL  ,
+	CONSTRAINT Module_PK PRIMARY KEY (Nom_Module)
 
-	,CONSTRAINT Module_Classe_FK FOREIGN KEY (id_classe) REFERENCES Classe(id_classe)
+	,CONSTRAINT Module_Classe_FK FOREIGN KEY (nom_classe) REFERENCES Classe(nom_classe)
 );
 
 
@@ -109,16 +103,15 @@ CREATE TABLE Module(
 -- Table: Cours
 ------------------------------------------------------------*/
 CREATE TABLE Cours(
-	id_cours    INT  NOT NULL ,
-	Nom_cours   VARCHAR (50) NOT NULL ,
-	id_salle    INT  NOT NULL ,
-	CIN         INT  NOT NULL ,
-	id_module   INT  NOT NULL  ,
-	CONSTRAINT Cours_PK PRIMARY KEY (id_cours)
+	Nom_cours    VARCHAR (50) NOT NULL ,
+	id_salle     INT  NOT NULL ,
+	CIN          INT  NOT NULL ,
+	Nom_Module   VARCHAR (30) NOT NULL  ,
+	CONSTRAINT Cours_PK PRIMARY KEY (Nom_cours)
 
 	,CONSTRAINT Cours_Salle_FK FOREIGN KEY (id_salle) REFERENCES Salle(id_salle)
 	,CONSTRAINT Cours_Professeur0_FK FOREIGN KEY (CIN) REFERENCES Professeur(CIN)
-	,CONSTRAINT Cours_Module1_FK FOREIGN KEY (id_module) REFERENCES Module(id_module)
+	,CONSTRAINT Cours_Module1_FK FOREIGN KEY (Nom_Module) REFERENCES Module(Nom_Module)
 );
 
 
@@ -128,34 +121,35 @@ CREATE TABLE Cours(
 CREATE TABLE PayementFrais(
 	numero_facture   INT  NOT NULL ,
 	typePayement     VARCHAR (10) NOT NULL ,
-	montatApayer     INT  NOT NULL  ,
+	montatApayer     INT  NOT NULL ,
+	CNE              INT  NOT NULL  ,
 	CONSTRAINT PayementFrais_PK PRIMARY KEY (numero_facture)
+
+	,CONSTRAINT PayementFrais_etudiant_FK FOREIGN KEY (CNE) REFERENCES etudiant(CNE)
 );
 
 
 /*------------------------------------------------------------
--- Table:  Etre assigner 
+-- Table: Connexion
 ------------------------------------------------------------*/
-CREATE TABLE Etre_assigner(
+CREATE TABLE Connexion(
+	Username   VARCHAR (50) NOT NULL ,
+	Password   VARCHAR (50) NOT NULL ,
+	Email      VARCHAR (50) NOT NULL  ,
+	CONSTRAINT Connexion_PK PRIMARY KEY (Username)
+);
+
+
+/*------------------------------------------------------------
+-- Table: assigner
+------------------------------------------------------------*/
+CREATE TABLE assigner(
 	id_salle   INT  NOT NULL ,
 	CIN        INT  NOT NULL  ,
-	CONSTRAINT Etre_assigner_PK PRIMARY KEY (id_salle,CIN)
+	CONSTRAINT assigner_PK PRIMARY KEY (id_salle,CIN)
 
-	,CONSTRAINT Etre_assigner_Salle_FK FOREIGN KEY (id_salle) REFERENCES Salle(id_salle)
-	,CONSTRAINT Etre_assigner_Professeur0_FK FOREIGN KEY (CIN) REFERENCES Professeur(CIN)
-);
-
-
-/*------------------------------------------------------------
--- Table: Payer
-------------------------------------------------------------*/
-CREATE TABLE Payer(
-	CNE              INT  NOT NULL ,
-	numero_facture   INT  NOT NULL  ,
-	CONSTRAINT Payer_PK PRIMARY KEY (CNE,numero_facture)
-
-	,CONSTRAINT Payer_etudiant_FK FOREIGN KEY (CNE) REFERENCES etudiant(CNE)
-	,CONSTRAINT Payer_PayementFrais0_FK FOREIGN KEY (numero_facture) REFERENCES PayementFrais(numero_facture)
+	,CONSTRAINT assigner_Salle_FK FOREIGN KEY (id_salle) REFERENCES Salle(id_salle)
+	,CONSTRAINT assigner_Professeur0_FK FOREIGN KEY (CIN) REFERENCES Professeur(CIN)
 );
 
 

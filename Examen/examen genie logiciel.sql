57617670f1ed46ee53e2c22b3ca4af67544a8ab7
Joy Daniel <88509670+Danieljucke@users.noreply.github.com>
2022-05-27 00:18:21 +0100

    Add files via upload

--- Examen/examen genie logiciel.sql
+++ Examen/examen genie logiciel.sql
@@ -1,8 +1,8 @@
 /*------------------------------------------------------------
 *        Script SQLSERVER 
 ------------------------------------------------------------*/
-
-
+create database Examen
+use Examen
 /*------------------------------------------------------------
 -- Table: Professeur
 ------------------------------------------------------------*/
@@ -37,8 +37,9 @@ CREATE TABLE Salle(
 -- Table: Options
 ------------------------------------------------------------*/
 CREATE TABLE Options(
+	id_option    INT  NOT NULL ,
 	Nom_option   VARCHAR (30) NOT NULL  ,
-	CONSTRAINT Options_PK PRIMARY KEY (Nom_option)
+	CONSTRAINT Options_PK PRIMARY KEY (id_option)
 );
 
 
@@ -46,8 +47,9 @@ CREATE TABLE Options(
 -- Table: parcours
 ------------------------------------------------------------*/
 CREATE TABLE parcours(
+	id_parcours    INT  NOT NULL ,
 	nom_parcours   VARCHAR (10) NOT NULL  ,
-	CONSTRAINT parcours_PK PRIMARY KEY (nom_parcours)
+	CONSTRAINT parcours_PK PRIMARY KEY (id_parcours)
 );
 
 
@@ -66,12 +68,12 @@ CREATE TABLE etudiant(
 	Email               VARCHAR (50) NOT NULL ,
 	Frais               INT  NOT NULL ,
 	AnneeObtentionBac   VARCHAR (15) NOT NULL ,
-	Nom_option          VARCHAR (30) NOT NULL ,
-	nom_parcours        VARCHAR (10) NOT NULL  ,
+	id_option           INT  NOT NULL ,
+	id_parcours         INT  NOT NULL  ,
 	CONSTRAINT etudiant_PK PRIMARY KEY (CNE)
 
-	,CONSTRAINT etudiant_Options_FK FOREIGN KEY (Nom_option) REFERENCES Options(Nom_option)
-	,CONSTRAINT etudiant_parcours0_FK FOREIGN KEY (nom_parcours) REFERENCES parcours(nom_parcours)
+	,CONSTRAINT etudiant_Options_FK FOREIGN KEY (id_option) REFERENCES Options(id_option)
+	,CONSTRAINT etudiant_parcours0_FK FOREIGN KEY (id_parcours) REFERENCES parcours(id_parcours)
 );
 
 
@@ -79,9 +81,10 @@ CREATE TABLE etudiant(
 -- Table: Classe
 ------------------------------------------------------------*/
 CREATE TABLE Classe(
+	id_classe       INT  NOT NULL ,
 	nom_classe      VARCHAR (20) NOT NULL ,
 	niveau_classe   VARCHAR (20) NOT NULL  ,
-	CONSTRAINT Classe_PK PRIMARY KEY (nom_classe)
+	CONSTRAINT Classe_PK PRIMARY KEY (id_classe)
 );
 
 
@@ -89,13 +92,14 @@ CREATE TABLE Classe(
 -- Table: Module
 ------------------------------------------------------------*/
 CREATE TABLE Module(
+	id_module          INT  NOT NULL ,
 	Nom_Module         VARCHAR (30) NOT NULL ,
 	Moyenne_generale   INT  NOT NULL ,
 	charge_horaire     INT  NOT NULL ,
-	nom_classe         VARCHAR (20) NOT NULL  ,
-	CONSTRAINT Module_PK PRIMARY KEY (Nom_Module)
+	id_classe          INT  NOT NULL  ,
+	CONSTRAINT Module_PK PRIMARY KEY (id_module)
 
-	,CONSTRAINT Module_Classe_FK FOREIGN KEY (nom_classe) REFERENCES Classe(nom_classe)
+	,CONSTRAINT Module_Classe_FK FOREIGN KEY (id_classe) REFERENCES Classe(id_classe)
 );
 
 
@@ -103,15 +107,16 @@ CREATE TABLE Module(
 -- Table: Cours
 ------------------------------------------------------------*/
 CREATE TABLE Cours(
-	Nom_cours    VARCHAR (50) NOT NULL ,
-	id_salle     INT  NOT NULL ,
-	CIN          INT  NOT NULL ,
-	Nom_Module   VARCHAR (30) NOT NULL  ,
-	CONSTRAINT Cours_PK PRIMARY KEY (Nom_cours)
+	id_cours    INT  NOT NULL ,
+	Nom_cours   VARCHAR (50) NOT NULL ,
+	id_salle    INT  NOT NULL ,
+	CIN         INT  NOT NULL ,
+	id_module   INT  NOT NULL  ,
+	CONSTRAINT Cours_PK PRIMARY KEY (id_cours)
 
 	,CONSTRAINT Cours_Salle_FK FOREIGN KEY (id_salle) REFERENCES Salle(id_salle)
 	,CONSTRAINT Cours_Professeur0_FK FOREIGN KEY (CIN) REFERENCES Professeur(CIN)
-	,CONSTRAINT Cours_Module1_FK FOREIGN KEY (Nom_Module) REFERENCES Module(Nom_Module)
+	,CONSTRAINT Cours_Module1_FK FOREIGN KEY (id_module) REFERENCES Module(id_module)
 );
 
 
@@ -133,10 +138,11 @@ CREATE TABLE PayementFrais(
 -- Table: Connexion
 ------------------------------------------------------------*/
 CREATE TABLE Connexion(
-	Username   VARCHAR (50) NOT NULL ,
-	Password   VARCHAR (50) NOT NULL ,
-	Email      VARCHAR (50) NOT NULL  ,
-	CONSTRAINT Connexion_PK PRIMARY KEY (Username)
+	id_compte   INT  NOT NULL ,
+	Username    VARCHAR (50) NOT NULL ,
+	Password    VARCHAR (50) NOT NULL ,
+	Email       VARCHAR (50) NOT NULL  ,
+	CONSTRAINT Connexion_PK PRIMARY KEY (id_compte)
 );
 
 
@@ -153,4 +159,16 @@ CREATE TABLE assigner(
 );
 
 
+/*------------------------------------------------------------------------------------
+-- Trigger : pour table etudiant 
+------------------------------------------------------------------------------------*/
+create trigger tg_updateFrais 
+on PayementFrais
+after insert 
+as declare @montant bigint 
+set @montant= (select montatApayer from inserted)
+update etudiant set 
+Frais =Frais+@Montant 
+where CNE=(select CNE from inserted)
 
+insert into Connexion values (1,'admin','1234567','@Admin')

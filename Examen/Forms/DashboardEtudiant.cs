344082b4d2d7470b3bfe7495c70b6be972e05d90
LeonardoRubuz <98391205+LeonardoRubuz@users.noreply.github.com>
2022-05-27 08:43:41 +0100

    Add files via upload

--- Examen/Forms/DashboardEtudiant.cs
+++ Examen/Forms/DashboardEtudiant.cs
@@ -1,3 +1,103 @@
+<<<<<<< HEAD
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Examen.Forms
+{
+    public partial class DashboardEtudiant : Form
+    {
+        public DashboardEtudiant()
+        {
+            InitializeComponent();
+        }
+
+        private Form activePanel = null;
+        private void openPanel(Form panelform)
+        {
+            if (activePanel != null)
+                activePanel.Close();
+            activePanel = panelform;
+            panelform.TopLevel = false;
+            panelform.Dock = DockStyle.Fill;
+            ContainerPanel.Controls.Add(panelform);
+            ContainerPanel.Tag = panelform;
+            panelform.Show();
+        }
+
+        private void btnCours_Click(object sender, EventArgs e)
+        {
+            pnlNav.Height = btnCours.Height;
+            pnlNav.Top = btnCours.Top;
+            pnlNav.Left = btnCours.Left;
+            btnCours.BackColor = Color.FromArgb(46, 51, 73);
+            openPanel(new CoursPanel());
+        }
+
+        private void btnProf_Click(object sender, EventArgs e)
+        {
+            pnlNav.Height = btnProf.Height;
+            pnlNav.Top = btnProf.Top;
+            pnlNav.Left = btnProf.Left;
+            btnProf.BackColor = Color.FromArgb(46, 51, 73);
+            openPanel(new ProfPanel());
+        }
+
+        private void btnCours_Leave(object sender, EventArgs e)
+        {
+            btnCours.BackColor = Color.FromArgb(24, 30, 54);
+        }
+
+        private void btnSalle_Click(object sender, EventArgs e)
+        {
+            pnlNav.Height = btnSalle.Height;
+            pnlNav.Top = btnSalle.Top;
+            pnlNav.Left = btnSalle.Left;
+            btnSalle.BackColor = Color.FromArgb(46, 51, 73);
+            openPanel(new SallePanel());
+        }
+
+        private void btnProf_Leave(object sender, EventArgs e)
+        {
+            btnProf.BackColor = Color.FromArgb(24, 30, 54);
+        }
+
+        private void btnSalle_Leave(object sender, EventArgs e)
+        {
+            btnSalle.BackColor = Color.FromArgb(24, 30, 54);
+        }
+
+        private void CloseBtn_MouseHover(object sender, EventArgs e)
+        {
+            CloseBtn.BackColor = Color.FromArgb(184, 15, 10);
+        }
+
+        private void CloseBtn_Leave(object sender, EventArgs e)
+        {
+            CloseBtn.BackColor = Color.FromArgb(41, 56, 73);
+        }
+
+        private void MaxBtn_Click(object sender, EventArgs e)
+        {
+            this.WindowState = FormWindowState.Maximized;
+        }
+
+        private void MinBtn_Click(object sender, EventArgs e)
+        {
+            this.WindowState = FormWindowState.Minimized;
+        }
+
+        private void CloseBtn_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
+=======
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel;
@@ -55,3 +155,4 @@ namespace Examen.Forms
 
     }
 }
+>>>>>>> 27d8ca376d0e8165491d43fbf244a84f1eacbebd
--- Examen/Forms/DashboardProf.cs
+++ Examen/Forms/DashboardProf.cs
@@ -1,3 +1,114 @@
+<<<<<<< HEAD
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Examen.Forms
+{
+    public partial class DashboardProf : Form
+    {
+        public DashboardProf()
+        {
+            InitializeComponent();
+        }
+
+        private Form activePanel = null;
+        private void openPanel(Form panelform)
+        {
+            if (activePanel != null)
+                activePanel.Close();
+            activePanel = panelform;
+            panelform.TopLevel = false;
+            panelform.Dock = DockStyle.Fill;
+            MainPanel.Controls.Add(panelform);
+            MainPanel.Tag = panelform;
+            panelform.Show();
+        }
+
+        private void btnCours_Click(object sender, EventArgs e)
+        {
+            pnlNav.Height = btnCours.Height;
+            pnlNav.Top = btnCours.Top;
+            pnlNav.Left = btnCours.Left;
+            btnCours.BackColor = Color.FromArgb(46, 51, 73);
+            openPanel(new CoursPanel());
+        }
+
+        private void btnSalle_Click(object sender, EventArgs e)
+        {
+            pnlNav.Height = btnSalle.Height;
+            pnlNav.Top = btnSalle.Top;
+            pnlNav.Left = btnSalle.Left;
+            btnSalle.BackColor = Color.FromArgb(46, 51, 73);
+            openPanel(new SallePanel());
+        }
+
+        private void btnEtud_Click(object sender, EventArgs e)
+        {
+            pnlNav.Height = btnEtud.Height;
+            pnlNav.Top = btnEtud.Top;
+            pnlNav.Left = btnEtud.Left;
+            btnEtud.BackColor = Color.FromArgb(46, 51, 73);
+            openPanel(new EtudiantPanel());
+        }
+
+        private void btnCours_Leave(object sender, EventArgs e)
+        {
+            btnCours.BackColor = Color.FromArgb(24, 30, 54);
+        }
+
+        private void btnEtud_Leave(object sender, EventArgs e)
+        {
+            btnEtud.BackColor = Color.FromArgb(24, 30, 54);
+        }
+
+        private void btnSalle_Leave(object sender, EventArgs e)
+        {
+            btnSalle.BackColor = Color.FromArgb(24, 30, 54);
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            this.WindowState = FormWindowState.Maximized;
+        }
+
+        private void minBtn_Click(object sender, EventArgs e)
+        {
+            this.WindowState = FormWindowState.Minimized;
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            
+
+        }
+
+        private void button4_Click_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void button5_Leave(object sender, EventArgs e)
+        {
+            button5.BackColor = Color.FromArgb(41, 56, 73);
+        }
+
+        private void button5_MouseHover(object sender, EventArgs e)
+        {
+            button5.BackColor = Color.FromArgb(184, 15, 10);
+        }
+    }
+}
+=======
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel;
@@ -76,3 +187,4 @@ namespace Examen.Forms
         }
     }
 }
+>>>>>>> 27d8ca376d0e8165491d43fbf244a84f1eacbebd

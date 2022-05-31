3646f1b8e9f633fb6357881d3fd560eeb4e947ce
Joy Daniel <88509670+Danieljucke@users.noreply.github.com>
2022-05-31 19:25:07 +0100

    Add files via upload

--- Examen/Properties/Resources.Designer.cs
+++ Examen/Properties/Resources.Designer.cs
@@ -179,5 +179,15 @@ namespace Examen.Properties {
                 return ((System.Drawing.Bitmap)(obj));
             }
         }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap school_icon_png_14039 {
+            get {
+                object obj = ResourceManager.GetObject("school-icon-png-14039", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
     }
 }

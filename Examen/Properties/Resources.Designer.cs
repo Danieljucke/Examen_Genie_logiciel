7fd3e7d3edc3050ef5aad3f329b138827186d43b
Joy Daniel <88509670+Danieljucke@users.noreply.github.com>
2022-06-01 12:29:39 +0100

    Add files via upload

--- Examen/Properties/Resources.Designer.cs
+++ Examen/Properties/Resources.Designer.cs
@@ -189,5 +189,15 @@ namespace Examen.Properties {
                 return ((System.Drawing.Bitmap)(obj));
             }
         }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap search {
+            get {
+                object obj = ResourceManager.GetObject("search", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
     }
 }
